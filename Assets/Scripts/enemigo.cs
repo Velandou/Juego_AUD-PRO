e1979a9f1cddadbe78f5971c60c11e17c01ac653
Velandou <est.santiago.velan@unimilitar.edu.co>
2024-05-07 23:42:00 -0600

    Mov Enemigo - Nivel 1

--- Assets/Scripts/enemigo.cs
+++ Assets/Scripts/enemigo.cs
@@ -4,6 +4,51 @@ using UnityEngine;
 
 public class enemigo : MonoBehaviour
 {
+	public Rigidbody2D rb2D;
+
+	public float velocidadDeMovimiento;
+
+	public LayerMask capaAbajo;
+	public LayerMask capaEnFrente;
+
+	public float distanciaAbajo;
+	public float distanciaEnFrente;
+
+	public Transform ControladorAbajo;
+	public Transform ControladorEnFrente;
+
+	public bool informacionAbajo;
+	public bool informacionEnFrente;
+
+	private bool mirandoALaDerecha = true;
+
+	private void Update()
+    {
+		rb2D.velocity = new Vector2(velocidadDeMovimiento,rb2D.velocity.y);
+
+		informacionEnFrente = Physics2D.Raycast(ControladorEnFrente.position, transform.right, distanciaEnFrente, capaEnFrente);
+		informacionAbajo = Physics2D.Raycast(ControladorAbajo.position, transform.up * -1, distanciaAbajo, capaAbajo);
+
+		if(informacionEnFrente || !informacionAbajo)
+        {
+			Girar();
+        }
+	}
+
+	private void Girar()
+    {
+		mirandoALaDerecha = !mirandoALaDerecha;
+		transform.eulerAngles = new Vector3(0, transform.eulerAngles.y + 180, 0);
+		velocidadDeMovimiento *= -1;
+    }
+
+	private void OnDrawGizmos()
+    {
+		Gizmos.color = Color.red;
+		Gizmos.DrawLine(ControladorAbajo.transform.position, ControladorAbajo.transform.position + transform.up * -1 * distanciaAbajo);
+		Gizmos.DrawLine(ControladorEnFrente.transform.position, ControladorEnFrente.transform.position + transform.right * distanciaEnFrente);
+	}
+
 	private void OnCollisionEnter2D(Collision2D other)
 	{
 		if (other.gameObject.CompareTag("Player"))
